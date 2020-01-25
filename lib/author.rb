a1625c0ebb5c02a4d24ac0b0086ec021674015f0
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 17:36:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,24 +1,24 @@
-class Artist
+class Author
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
-  def add_song(song)
-    song.artist = self
+  def add_post(post)
+    post.author = self
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
   end
 
-  def self.song_count
-    Song.all.count
+  def self.post_count
+    Post.all.count
   end
 end
